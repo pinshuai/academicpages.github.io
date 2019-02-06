d7b6af7dd0fc8b3188a4aa7ed4fff1e95a0dac23
pshuai88 <pshuai88@gmail.com>
2019-02-06 12:35:41 -0800

    update publication list

--- markdown_generator/auto_generate.sh
+++ markdown_generator/auto_generate.sh
@@ -4,7 +4,7 @@ jupyter nbconvert --to notebook --inplace --execute publications.ipynb
 
 echo "push changes to github"
 git pull origin master
-git add .
+git add --all
 git commit -m 'update publication list'
 git push origin master
 echo "Done!"
