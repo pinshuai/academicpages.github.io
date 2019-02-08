385e24a8715b8d1bf7c940f643388585059e895d
Pin Shuai <pshuai88@gmail.com>
2019-02-08 12:12:25 -0800

    $1

---
+++ cv/auto_generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "--execute publications.ipynb"
+jupyter nbconvert --to notebook --inplace --execute publications.ipynb 
+
+echo "--push changes to github"
+git pull origin master
+git add --all
+git commit -m 'update publication list'
+git push origin master
+echo "--Done!"
+
+
