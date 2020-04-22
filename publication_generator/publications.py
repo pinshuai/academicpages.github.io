a57002e4b2ec6e17d2b05cee379e2b0ebe2bfa19
Pin Shuai <pshuai88@gmail.com>
2020-04-22 14:26:50 -0700

    add Zachara 2020 WRR paper to the publication list; update publication_generator readme file

--- publication_generator/publications.py
+++ publication_generator/publications.py
@@ -1,9 +1,9 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
 # # Publications markdown generator for academicpages
 # 
-# Takes a TSV of publications with metadata and converts them for use with [academicpages.github.io](academicpages.github.io). This is an interactive Jupyter notebook, with the core python code in publications.py. Run either from the `markdown_generator` folder after replacing `publications.tsv` with one that fits your format.
+# Takes a TSV of publications with metadata and converts them for use with [academicpages.github.io](academicpages.github.io). This is an interactive Jupyter notebook ([see more info here](http://jupyter-notebook-beginner-guide.readthedocs.io/en/latest/what_is_jupyter.html)). The core python code is also in `publications.py`. Run either from the `markdown_generator` folder after replacing `publications.tsv` with one containing your data.
 # 
 # TODO: Make this work with BibTex and other databases of citations, rather than Stuart's non-standard TSV format and citation style.
 # 
@@ -15,13 +15,21 @@
 # - `excerpt` and `paper_url` can be blank, but the others must have values. 
 # - `pub_date` must be formatted as YYYY-MM-DD.
 # - `url_slug` will be the descriptive part of the .md file and the permalink URL for the page about the paper. The .md file will be `YYYY-MM-DD-[url_slug].md` and the permalink will be `https://[yourdomain]/publications/YYYY-MM-DD-[url_slug]`
+# 
+# This is how the raw file looks (it doesn't look pretty, use a spreadsheet or other program to edit and create).
+
+# In[1]:
+
+
+# !cat publications.tsv
 
 
 # ## Import pandas
 # 
 # We are using the very handy pandas library for dataframes.
 
-# In[2]:
+# In[1]:
+
 
 import pandas as pd
 
@@ -32,9 +40,11 @@ import pandas as pd
 # 
 # I found it important to put this data in a tab-separated values format, because there are a lot of commas in this kind of data and comma-separated values can get messed up. However, you can modify the import statement, as pandas also has read_excel(), read_json(), and others.
 
-# In[3]:
+# In[7]:
 
-publications = pd.read_csv("publications.tsv", sep="\t", header=0)
+
+# publications = pd.read_csv("publications.tsv", sep="\t", header=0)
+publications = pd.read_csv("./publications.csv")
 publications
 
 
@@ -42,7 +52,8 @@ publications
 # 
 # YAML is very picky about how it takes a valid string, so we are replacing single and double quotes (and ampersands) with their HTML encoded equivilents. This makes them look not so readable in raw format, but they are parsed and rendered nicely.
 
-# In[4]:
+# In[3]:
+
 
 html_escape_table = {
     "&": "&amp;",
@@ -57,9 +68,10 @@ def html_escape(text):
 
 # ## Creating the markdown files
 # 
-# This is where the heavy lifting is done. This loops through all the rows in the TSV dataframe, then starts to concatentate a big string (```md```) that contains the markdown for each type. It does the YAML metadata first, then does the description for the individual page. If you don't want something to appear (like the "Recommended citation")
+# This is where the heavy lifting is done. This loops through all the rows in the TSV dataframe, then starts to concatentate a big string (```md```) that contains the markdown for each type. It does the YAML metadata first, then does the description for the individual page.
 
-# In[5]:
+# In[8]:
+
 
 import os
 for row, item in publications.iterrows():
@@ -91,12 +103,12 @@ for row, item in publications.iterrows():
     md += "\n---"
     
     ## Markdown description for individual page
-    
-    if len(str(item.paper_url)) > 5:
-        md += "\n\n<a href='" + item.paper_url + "'>Download paper here</a>\n" 
         
-    if len(str(item.excerpt)) > 5:
-        md += "\n" + html_escape(item.excerpt) + "\n"
+#     if len(str(item.excerpt)) > 5:
+#         md += "\n" + html_escape(item.excerpt) + "\n"
+    md += "\nAbstract: " + html_escape(item.abstract) + "\n"
+    if len(str(item.paper_url)) > 5:
+        md += "\n[Download paper here](" + item.paper_url + ")\n" 
         
     md += "\nRecommended citation: " + item.citation
     
@@ -105,4 +117,17 @@ for row, item in publications.iterrows():
     with open("../_publications/" + md_filename, 'w') as f:
         f.write(md)
 
+
+# These files are in the publications directory, one directory below where we're working from.
+
+# In[6]:
+
+
+# !ls ../_publications/
+
+
+# In[7]:
+
+
+# !cat ../_publications/2009-10-01-paper-title-number-1.md
 
--- publication_generator/readme.md
+++ publication_generator/readme.md
@@ -2,6 +2,16 @@
 
 These .ipynb files are Jupyter notebook files that convert a TSV containing structured data about talks (`talks.tsv`) or presentations (`presentations.tsv`) into individual markdown files that will be properly formatted for the academicpages template. The notebooks contain a lot of documentation about the process. The .py files are pure python that do the same things if they are executed in a terminal, they just don't have pretty documentation.
 
+1. Add new entry in the `publication.csv` file.
 
+2. Populate publication markdown files.
 
+To execute in the command line:
 
+```
+cp ./src/publication.py .
+
+python3 publication.py
+```
+
+note: the first column of `publication.csv` may need to be in `YYYY-MM-DD` date format to properly render in the website.
